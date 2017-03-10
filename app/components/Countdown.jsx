a4150929a3e99f39aa9264ba0e294b61d6831de8
Adebiyi Oluwatobi <tobiemmanuel44@yahoo.com>
2017-03-09 23:15:17 -0500

    Added Routes and Container Components

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Countdown = (props) => {
+  return (
+    <div>
+      <div>
+        <p>CountDown.jsx</p>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = (props) => {
+  return (
+    <div>
+      <div>
+        <p>Timer.jsx</p>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Timer;
