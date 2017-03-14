5c14cbede8f122b64204a4d7d61001dfca7c05c8
Adebiyi Oluwatobi <tobiemmanuel44@yahoo.com>
2017-03-14 15:38:30 -0400

    Learning Component Lifecycle

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -25,6 +25,10 @@ var Countdown = React.createClass({
       }
     }
   },
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
   startTimer: function () {
     this.timer = setInterval(() => {
       var newCount = this.state.count - 1;
@@ -32,6 +36,12 @@ var Countdown = React.createClass({
         count: newCount >= 0 ? newCount : 0
       });
     }, 1000);
+
+    if (newCount === 0) {
+      this.setState({
+        countdownStatus: 'stopped'
+      });
+    }
   },
   handleSetCountdown: function (seconds) {
     this.setState({
