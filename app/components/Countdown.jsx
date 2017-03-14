102e5e8af207cea04caff18b396ba35cf83267a4
Adebiyi Oluwatobi <tobiemmanuel44@yahoo.com>
2017-03-13 20:50:59 -0400

    Started the Count Down

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -4,11 +4,32 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
   getInitialState: function () {
-    return {count: 0};
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus != prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
   },
   handleSetCountdown: function (seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started'
     });
   },
   render: function () {
@@ -17,7 +38,7 @@ var Countdown = React.createClass({
     return (
       <div>
         <div>
-          <Clock totalSeconds={129}/>
+          <Clock totalSeconds={count}/>
           <CountdownForm onSetCountdown={this.handleSetCountdown}/>
         </div>
       </div>
